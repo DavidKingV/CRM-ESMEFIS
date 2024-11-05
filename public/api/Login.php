e695e52862c1d056d9982874028e418f16e3f6f7
David Vega <qet135ui@hotmail.com>
2024-11-04 18:07:36 -0600

    correccion de corns

--- public/api/Login.php
+++ public/api/Login.php
@@ -4,6 +4,10 @@ require_once __DIR__ . '/../../backend/controllers/SessionController.php';
 
 use Vendor\Esmefis\DBConnection;
 
+header("Access-Control-Allow-Origin: *"); // Permite acceso desde cualquier dominio
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // Métodos permitidos
+header("Access-Control-Allow-Headers: Content-Type");
+
 $inputData = json_decode(file_get_contents('php://input'), true);
 
 $connection = new DBConnection();
