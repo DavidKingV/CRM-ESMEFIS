cb183d13a0065375390cf229cade96446a59a5cc
David Vega <qet135ui@hotmail.com>
2024-11-04 18:18:23 -0600

    modified

--- public/api/Login.php
+++ public/api/Login.php
@@ -2,6 +2,11 @@
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../backend/controllers/SessionController.php';
 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 use Vendor\Esmefis\DBConnection;
 
 header("Access-Control-Allow-Origin: *"); // Permite acceso desde cualquier dominio
